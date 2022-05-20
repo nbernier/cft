e7e5aae33f923bc6b5d17c2f223ca98db22af54b
nbernier <nicolas.bernier@perficient.com>
2022-05-20 11:11:55 -0400

    wip

--- env.php
+++ env.php
@@ -7,8 +7,8 @@ echo("Current Directory is " . $cur_dir);
 
 var_dump(getenv());
 
-echo var_dump(getenv())
-echo $_ENV
+echo "var_dump(getenv())"
+echo "$_ENV"
 while (list($var,$value) = each ($_ENV)) {
     echo "$var => $value <br />";
 }
