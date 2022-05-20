813c43fffa8e9e905407c1bf8e2c965fb9a81e40
nbernier <nicolas.bernier@perficient.com>
2022-05-20 11:50:45 -0400

    wip

--- env.php
+++ env.php
@@ -4,10 +4,19 @@ $title = "TITLE";
 $cur_dir = getcwd();
   
 echo("Current Directory is " . $cur_dir);
+$files1 = scandir("/app");
+
+print_r($files1);
 
 echo getenv('SHELL');
+echo "$_ENV";
 foreach (getenv() as $key => $value) {
     echo $key . ' - ' . $value;
+}
+
+
+while (list($var,$value) = each ($_ENV)) {
+    echo "$var => $value <br />";
 }
 echo ("done")
 ?>
